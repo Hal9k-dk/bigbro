46c6ca775af5f7e894eee3ba25603476c562aa31
Jacob Jenner Rasmussen <jacob@jener.dk>
2018-09-29 19:09:33 +0200

    Began adding documentation
    Added doxygen config file.
    Documented main.cpp and base.h.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -4,14 +4,25 @@
 #include <controllers/camera.h>
 #include <controllers/printer.h>
 
+/**
+ * The active controller instance.
+ * BaseController doesn't currently support swapping this at runtime.
+ */
 BaseController* controller;
 
+/**
+ * Initialization codfe 
+ */
 void setup()
 {
 	//controller = new ACSController();
 	controller = new PrinterController();
 	//controller = new CameraController();
 }
+
+/**
+ * Main control loop
+ */
 void loop()
 {
 	controller->update();
