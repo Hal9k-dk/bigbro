0d0d6dca8e35f58d88505a5449f9fb4b5b08d046
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2019-03-19 18:27:28 +0100

    Implemented main page for doxygen

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,6 +3,24 @@
 #include <controllers/camera.h>
 #include <controllers/printer.h>
 
+/*! @mainpage Documentation for the BigBro firmware
+	The BigBro firmware is part of the access system used at Hal9k
+	It runs on the access boxes attached to different restricted machines.
+
+	The boxes enable or remove power from the machines, depending on access permissions.
+
+	There are two main types of boxes the users will interact with.
+	* The basic access control box, implemented in ACSController, enables power to machines, 
+	  as long as an approved card is present.
+	* The printer access control box, implemented in PrinterController, 
+	  enables power to the machines if a valid card is present,
+	  or if a print, started with an approved card, is currently in progress.
+	  The hardware difference between the two boxes is a higher current capability on the printer box,
+	  and a current sensor, as the printer box monitors current consumption of the printers, and therefore
+	  passes the entire current for the machine.
+	  The basic access control boxes instead use a much smaller relay, to control an external contactor, powering the machine.
+*/
+
 /**
  * The active controller instance.
  * BaseController doesn't currently support swapping this at runtime.
