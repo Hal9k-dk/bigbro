8ae1f29bd71ba22a24d455d4952f7ff3660981fc
Torsten Martinsen <torsten@bullestock.net>
2023-07-02 11:42:52 +0200

    Achieve perfection.

--- cad/display.py
+++ cad/display.py
@@ -1,6 +1,6 @@
 import cadquery as cq
 
-height = 50
+height = 35
 width = 50
 thickness = 2
 centerXY = (True, True, False)
@@ -13,20 +13,21 @@ result = (cq.Workplane("XY")
 # display hole
 # TODO: Adjust position/size
 disp_y_offset = 0
-disp_x_offset = 6
+disp_x_offset = 5
 dhh = 27
 dhw = 28
 # display size
 dh = 30
-dw = 41
+dw = 40.5
 w1 = 3
-h2 = 6.8
+h2 = 6.3
 disphole = (cq.Workplane("XY")
             .transformed(offset=(disp_x_offset, disp_y_offset, 0))
             .box(dhw+2*thickness, dhh+2*thickness, thickness, centerXY)
             .edges(">Z")
             .chamfer(thickness*0.99, 2)
             )
+rod_d = 1
 result = (result.cut(disphole)
        .workplaneFromTagged("origin")
        .transformed(offset=(dw/2+w1/2, 0, thickness))
@@ -34,14 +35,14 @@ result = (result.cut(disphole)
        .workplaneFromTagged("origin")
        .transformed(offset=(dw/2+w1/2, dh/2, thickness+h2-1),
                     rotate=(90, 0, 0))
-       .circle(1).extrude(dh)
+       .circle(rod_d/2).extrude(dh)
        .workplaneFromTagged("origin")
        .transformed(offset=(-dw/2-1.5*w1, 0, thickness))
        .box(w1, dh, h2, centered=(False, True, False))
        .workplaneFromTagged("origin")
        .transformed(offset=(-dw/2-0.5*w1, dh/2, thickness+h2-1),
                     rotate=(90, 0, 0))
-       .circle(1).extrude(dh)
+       .circle(rod_d/2).extrude(dh)
 )
 
 show_object(result)
