95cf0a247d288bca099aaed8bab91dae31c674c5
Torsten Martinsen <torsten@bullestock.net>
2026-06-28 12:22:37 +0200

    Align version number with HW revision.

--- esp32/flash_all.sh
+++ esp32/flash_all.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-esptool.py --chip esp32 -p /dev/ttyUSB0 -b 460800 --before=no-reset --no-stub --after=hard-reset write-flash --flash-mode dio --flash-freq 40m --flash-size 4MB 0x1000 build/bootloader/bootloader.bin 0x10000 build/bigbro.bin 0x8000 build/partition_table/partition-table.bin 0xd000 build/ota_data_initial.bin 0x150000 build/font.bin
+esptool --chip esp32 -p /dev/ttyUSB0 -b 460800 --before=no-reset --no-stub --after=hard-reset write-flash --flash-mode dio --flash-freq 40m --flash-size 4MB 0x1000 build/bootloader/bootloader.bin 0x10000 build/bigbro.bin 0x8000 build/partition_table/partition-table.bin 0xd000 build/ota_data_initial.bin 0x150000 build/font.bin
--- esp32/flash_app.sh
+++ esp32/flash_app.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-esptool.py --chip esp32 -p /dev/ttyUSB0 -b 460800 --before=no-reset --after=hard-reset write-flash --flash-mode dio --flash-freq 40m --flash-size 4MB 0x10000 build/bigbro.bin
+esptool --chip esp32 -p /dev/ttyUSB0 -b 460800 --before=no-reset --after=hard-reset write-flash --flash-mode dio --flash-freq 40m --flash-size 4MB 0x10000 build/bigbro.bin
